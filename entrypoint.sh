db81d23a01a36ad3557b7d0190190398cb7f6306
signingup <suchgelivable@gmail.com>
2024-07-31 11:41:19 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -m
+
+/usr/local/bin/mosdns start --as-service -d /etc/mosdns -c config.yaml &
 
 crond -f
-/usr/local/bin/mosdns start --as-service -d /etc/mosdns -c config.yaml
+fg %1
