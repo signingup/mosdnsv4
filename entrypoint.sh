33c532922f938e03c2eff841b542d3823fd22cab
signingup <suchgelivable@gmail.com>
2024-08-01 10:44:42 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -m
+FILE="/etc/mosdns/start.sh"
+
+if [ -f "$FILE" ]; then
+  cp "$FILE" /usr/bin/start
+  chmod +x /usr/bin/start && /usr/bin/start
+fi
 
 /usr/local/bin/mosdns start --as-service -d /etc/mosdns -c config.yaml &
 
